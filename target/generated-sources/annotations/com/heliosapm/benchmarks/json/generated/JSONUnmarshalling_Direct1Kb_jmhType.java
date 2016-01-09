522d383c95f27c45ee8bdf6ee6da0840efc6841a
nickman <whitehead.nicholas@gmail.com>
2016-01-09 11:20:29 -0500

    DevSynch

--- target/generated-sources/annotations/com/heliosapm/benchmarks/json/generated/JSONUnmarshalling_Direct1Kb_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.heliosapm.benchmarks.json.generated;
-public class JSONUnmarshalling_Direct1Kb_jmhType extends JSONUnmarshalling_Direct1Kb_jmhType_B3 {
-}
-
--- target/generated-sources/annotations/com/heliosapm/benchmarks/json/generated/JSONUnmarshalling_Heap614Kb_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.heliosapm.benchmarks.json.generated;
-public class JSONUnmarshalling_Heap614Kb_jmhType extends JSONUnmarshalling_Heap614Kb_jmhType_B3 {
-}
-
